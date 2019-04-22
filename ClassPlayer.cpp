5ca2d9a4e45616a1d98506a63573ca57f5ccb3e5
Mushcat <49735301+Mushcat@users.noreply.github.com>
2019-04-22 19:11:40 -0400

    Update ClassPlayer.cpp

--- ClassPlayer.cpp
+++ ClassPlayer.cpp
@@ -8,41 +8,41 @@ Player::Player() {
 	score = 0;
 }
 
-//Initilize player with a Name
+					//Initilize player with a Name
 Player::Player(string nam){
 	name = nam;
 	hand = new Deck();
 }
 
-//showw score
+						//showw score
 Player::Player(Deck* currentHand, int currentScore) {
 	hand = currentHand;
 	score = currentScore;
 }
 
-//How many cards each player draw each new game
+						//How many cards each player draw each new game
 void Player::FirstHand(Deck *target) {
 	for (int k = 0; k < 7; k++) {
 		hand->addCard(target->drawCard());
 	}
 }
 
-//Show Players cards
+							//Show the Players cards
 void Player::ViewHand() {
 	hand->displayStack();
 }
 
-//Get the player's hand
+					//Get the player's hand
 Deck* Player::getHand(){
 	return hand;
 }
 
-//Show the players name
+						//Show the players name
 void Player::ShowName(){
 	cout << name << endl;
 }
 
-//Checks the players hand
+						//Checks the players hand
 bool Player::CheckHand() {
 	if (hand->deckSize() == 0) {
 		return true;
